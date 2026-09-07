2d65e3a2a8bdfd03b970ebcd8e3c4eafa548c8fe
rpinget <rpinget@uade.edu.ar>
2026-09-06 23:27:57 -0300

    Se agrega ListarUsuarios

--- src/main/java/com/tucasaca/tienda/controller/UsuarioController.java
+++ src/main/java/com/tucasaca/tienda/controller/UsuarioController.java
@@ -1,7 +1,10 @@
 package com.tucasaca.tienda.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +28,10 @@ public class UsuarioController {
     public ResponseEntity<UsuarioResponseDTO> registrarUsuario(@RequestBody UsuarioRegistroDTO registroDTO) {
         UsuarioResponseDTO usuario = usuarioService.registrarUsuario(registroDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioResponseDTO>> listarUsuarios() {
+        return ResponseEntity.ok(usuarioService.listarUsuarios());
     }
 }
--- src/main/java/com/tucasaca/tienda/service/UsuarioService.java
+++ src/main/java/com/tucasaca/tienda/service/UsuarioService.java
@@ -1,5 +1,7 @@
 package com.tucasaca.tienda.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.tucasaca.tienda.dto.UsuarioRegistroDTO;
@@ -27,5 +29,11 @@ public class UsuarioService {
         Usuario usuario = usuarioMapper.toEntity(registroDTO);
         Usuario usuarioGuardado = usuarioRepository.save(usuario);
         return usuarioMapper.toResponseDTO(usuarioGuardado);
+    }
+
+    public List<UsuarioResponseDTO> listarUsuarios() {
+        return usuarioRepository.findAll().stream()
+                .map(usuarioMapper::toResponseDTO)
+                .toList();
     }
 }
